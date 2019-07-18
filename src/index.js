3c27de3432a1f0516baf433dc78c4b639f298ba3
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-18 12:00:45 -0600

    Begins user story two

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ $(document).ready(() => {
   getTopWord()
 })
 
+$( "#break-down" ).click();
+
 function getTopWord() {
   fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
   .then(response => response.json())
@@ -14,4 +16,10 @@ function getTopWord() {
     $("#top-word").append(word[0])
     $("#top-word-count").append(count[0])
   })
+}
+
+
+function addWord() {
+  // get info from placeholder text area
+  // populate that in fetch
 }
