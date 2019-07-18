70ea4c638f7801f8654b5b34c5e111e71545b66a
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-18 11:32:39 -0600

    Moves fetch call login got index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  getTopWord()
 })
+
+function getTopWord() {
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+  .then(response => response.json())
+  .then(word_info => {
+    let word = Object.keys(word_info["word"])
+
+    $("#top-word-here").append(word[0])
+  })
+}
